5dcc78a6284ae3f23a952df8f298dcabdf6552dd
Pavel Zelenev <pakanzelenev@mail.ru>
2026-09-13 18:07:16 +0700

    create docker and application environment

---
+++ src/main/java/ru/zelenev/LearningManageSystem/LearningManageSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.zelenev.LearningManageSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningManageSystemApplication.class, args);
+    }
+
+}
